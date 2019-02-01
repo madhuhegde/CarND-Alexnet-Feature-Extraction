13b0630e87542bc665cea8edce5618f12cb428d2
Madhu Hegde <madhu.hegde@gmail.com>
2019-01-31 23:57:41 -0800

    v1

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -1,25 +1,75 @@
 import pickle
+import time
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
 from alexnet import AlexNet
 
-# TODO: Load traffic signs data.
+nb_classes = 43
+epochs = 2
+batch_size = 128
 
-# TODO: Split data into training and validation sets.
+try:
+    f = open("train.p","rb")
+    data = pickle.load(f)
+    f.close()
+except Exception as ex:
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+    message = template.format(type(ex).__name__, ex.args)
+    print(message)
 
-# TODO: Define placeholders and resize operation.
+X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
 
-# TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
-# NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
-# past this point, keeping the weights before and up to `fc7` frozen.
-# This also makes training faster, less work to do!
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int64, None)
+one_hot_y = tf.one_hot(y, nb_classes)
+resized = tf.image.resize_images(x, (227, 227))
+
+# Returns the second final layer of the AlexNet model,
+# this allows us to redo the last layer for the traffic signs
+# model.
+fc7 = AlexNet(resized, feature_extract=True)
 fc7 = tf.stop_gradient(fc7)
+shape = (fc7.get_shape().as_list()[-1], nb_classes)
+fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-2))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+logits = tf.matmul(fc7, fc8W) + fc8b
 
-# TODO: Add the final layer for traffic sign classification.
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_op = tf.reduce_mean(cross_entropy)
+opt = tf.train.AdamOptimizer()
+train_op = opt.minimize(loss_op, var_list=[fc8W, fc8b])
+init_op = tf.global_variables_initializer()
 
-# TODO: Define loss, training, accuracy operations.
-# HINT: Look back at your traffic signs project solution, you may
-# be able to reuse some the code.
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# TODO: Train and evaluate the feature extraction model.
+
+def evaluate(X_data, y_data, sess):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    #sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session() as sess:
+    sess.run(init_op)
+
+    for i in range(epochs):
+        # training
+        print("Training Epoch: ", i+1)
+        X_train, y_train = shuffle(X_train, y_train)
+      
+        for offset in range(0, X_train.shape[0], batch_size):
+            end = offset + batch_size
+            sess.run(train_op, feed_dict={x: X_train[offset:end], y: y_train[offset:end]})
+    
+        validation_accuracy = evaluate(X_val, y_val, sess)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        #print()
+        print("")
